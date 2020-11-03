3b9325aaead9d35df24b2b316d7f851f2a7169e6
Manuel Segovia <msegovia@mac.com>
2020-11-02 20:06:10 -0500

    fixed mongo connection

--- src/services/dbConfig.js
+++ src/services/dbConfig.js
@@ -1,5 +1,5 @@
 const DB = 'dwg-specs';
-const DB_URI = `mongodb+srv://msegovia:Chiquito57@cluster0.pwboz.mongodb.net/dwg-specs?retryWrites=true&w=majority`;
+const DB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@cluster0.pwboz.mongodb.net/dwg-specs?retryWrites=true&w=majority`;
 const DB_OPTIONS = {
   useUnifiedTopology: true,
   useNewUrlParser: true,
