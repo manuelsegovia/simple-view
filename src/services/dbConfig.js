08c7b3939322444899e753b4fbb0568ea4aedc04
Manuel Segovia <msegovia@mac.com>
2020-11-08 09:56:24 -0500

    changed mongodb uri

--- src/services/dbConfig.js
+++ src/services/dbConfig.js
@@ -1,5 +1,7 @@
 const DB = 'dwg-specs';
-const DB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@cluster0.pwboz.mongodb.net/dwg-specs?retryWrites=true&w=majority`;
+const DB_URI =
+  process.env.MONGODB_URI ||
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@cluster0.pwboz.mongodb.net/dwg-specs?retryWrites=true&w=majority`;
 const DB_OPTIONS = {
   useUnifiedTopology: true,
   useNewUrlParser: true,
